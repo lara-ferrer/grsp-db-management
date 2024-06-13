0d5df87aa43c2ba89cefe2abdebc6c131f16a8b5
lara-ferrer <larafpics@gmail.com>
2024-06-13 12:07:43 +0200

    remove unused schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,4 @@
 import blockContent from './block-content'
-import businessCategory from './business-category'
 import city from './city'
 import { review } from './components/review'
 import { schedule } from './components/schedule'
@@ -10,7 +9,6 @@ import { scheduleHours } from './components/schedule-hours'
 export const schemaTypes = [
   blockContent,
   business,
-  businessCategory,
   city,
   timeValue,
   schedule,
